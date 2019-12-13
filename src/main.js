e16cc65d1a751375f7573b9f9073afb50af75d80
KLong <15110232612@163.com>
2019-12-13 16:07:50 +0800

    no login

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
+import 'lib-flexible'
 import App from './App.vue'
 import router from './router'
+import Header from './components/Header/Header.vue'
 
-import 'lib-flexible'
-
+Vue.component('Header', Header)
 new Vue({
   // components: {
   //   App
