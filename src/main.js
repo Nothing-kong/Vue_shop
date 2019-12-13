9c9b10bbde05e6216a493d66a8470ae52d4ee0af
KLong <15110232612@163.com>
2019-12-13 20:32:01 +0800

    1

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import router from './router'
 import Header from './components/Header/Header.vue'
 
+//注册全局组件
 Vue.component('Header', Header)
 new Vue({
   // components: {
