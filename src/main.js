145261d6210dd07040b0a3c8179e5f572147a275
KLong <15110232612@163.com>
2019-12-17 23:47:20 +0800

    1

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import App from './App.vue'
 import router from './router'
 import store from './vuex/store'
 import Header from './components/Header/Header.vue'
+import './validate'
+import i18n from './i18n'
 
 //注册全局组件
 Vue.component('Header', Header)
@@ -13,6 +15,12 @@ new Vue({
   // },
   // template: '<App/>'
   render: h => h(App),
-  router, // 所有组件都能看到 $router和$route  <router-link> 和 <router-view/>
-  store,//所有组件都能看到store
+
+  // 所有组件都能看到 $router和$route  <router-link> 和 <router-view/>
+  router,
+
+  i18n,
+
+  //所有组件都能看到store
+  store
 }).$mount('#app')
